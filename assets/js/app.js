3bd13cd72b6f3edc2266a1c463ea17f9ad2908d6
Rubel7461 <developerrubel.me@gmail.com>
2022-08-04 15:47:42 +0600

    Upload Fiver Time Counter

---
+++ assets/js/app.js
@@ -0,0 +1,65 @@
+const fiver_timer = document.getElementById('fiver_timer');
+const msg = document.getElementById('msg');
+const alert_msg = document.getElementById('alert_msg');
+const a1 = document.getElementById('a1');
+const stop_alerm = document.getElementById('stop_alerm');
+const progress_bardiv = document.getElementById('progress_bardiv');
+const progress_bar = document.getElementById('progress_bar');
+const progress_bar_val = document.getElementById('progress_bar_val');
+
+let count;
+
+fiver_timer.onsubmit = (e) =>{
+  e.preventDefault();
+
+  clearInterval(count);
+
+  const form_data = new FormData(e.target);
+  const object_data = Object.fromEntries(form_data.entries());
+  const {date,times,index} = Object.fromEntries(form_data.entries());
+
+  // Get Times...here..
+
+    let start_time = Date.now();
+    let end_time = new Date(date + ' ' + times);
+  
+  // Get Times....
+
+  count = setInterval(() =>{
+
+    time_counter(date,times,msg,count,a1);
+
+    if (!date || !times) {
+      msg.innerHTML = '';
+      alert_msg.innerHTML = setaleart('All Field Are Required','danger');
+    }else{
+      alert_msg.innerHTML = '';
+    };
+
+    let per = counter_per(start_time, end_time);
+    progress_bardiv.style.height = '15px';
+    progress_bar.style.width = `${per}%`;
+
+    // Showing progressbar value...
+
+    progress_bar_val.innerHTML = `${per} %`;
+
+    if (per <= 50) {
+      progress_bar.style.backgroundColor = 'red';
+      progress_bar_val.style.color = 'red';
+    }else{
+      // progress_bar.style.backgroundColor = 'red';
+      progress_bar_val.style.color = 'green';
+    }
+    if (per <= 1) {
+      progress_bar.style.opacity = '0';
+      progress_bar_val.style.opacity = '0';
+      progress_bar_val.style.height = '0';
+    }
+
+  },1000);
+};
+stop_alerm.onclick = (e) =>{
+  e.preventDefault();
+  a1.pause();
+}
---
+++ assets/js/function.js
@@ -0,0 +1,48 @@
+/**
+ * Alear Founction....
+*/
+
+const setaleart = (msg,type = 'danger') =>{
+    return `<p class=" d-flex justify-content-between align-content-center alert alert-${type}">${msg}<button class="btn-close" data-bs-dismiss="alert"></button></p>`
+};
+
+/**
+ * Time Counter....
+*/
+const time_counter = (date,times,msg,interval = false,a1 = 0) =>{
+
+    let start_time = Date.now();
+    let end_time = new Date(date + ' ' + times);
+    let order_time = Math.floor(Math.abs((end_time.getTime() - start_time)));
+  
+    // Get Vlue from times....
+  
+    let t_secound = Math.floor(order_time / 1000);
+    let t_minute = Math.floor(t_secound / 60);
+    let t_hour = Math.floor(t_minute / 60);
+    let day = Math.floor(t_hour / 24);
+  
+    let hour = t_hour - (day * 24);
+    let minute = t_minute - (day * 24 * 60) - (hour * 60);
+    let second = t_secound - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60);
+  
+    // Showing...
+    msg.innerHTML = ` ${day} Days : ${hour} Hours : ${minute} Minute : ${second} Secound`;
+
+    if (t_secound <= 0) {
+      a1.play();
+      clearInterval(interval);
+    };
+};
+
+
+/**
+ * For Progress bar....
+*/
+
+const counter_per = (start_time,end_time) =>{
+   let total_dif = end_time - start_time;
+   let change_time = end_time - Date.now();
+
+   return Math.floor((100 * change_time) / total_dif );
+};
